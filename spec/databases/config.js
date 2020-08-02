591a2ab9ee843d7da7ee897e8063b6f5a9ab6f95
Matthew Peveler <matt.peveler@gmail.com>
2020-08-02 19:27:15 -0400

    use dsn to define db environments for tests
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- spec/databases/config.js
+++ spec/databases/config.js
@@ -1,43 +1,63 @@
-import url from 'url';
 import path from 'path';
+import { ConnectionString } from 'connection-string';
 
-export default {
-  postgresql: {
-    host: url.parse(process.env.POSTGRES_PORT || '').hostname,
-    port: parseInt(url.parse(process.env.POSTGRES_PORT || '').port, 10),
-    user: process.env.POSTGRES_ENV_POSTGRES_USER,
-    password: process.env.POSTGRES_ENV_POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_ENV_POSTGRES_DB,
-  },
-  mysql: {
-    host: url.parse(process.env.MYSQL_PORT || '').hostname,
-    port: parseInt(url.parse(process.env.MYSQL_PORT || '').port, 10),
-    user: process.env.MYSQL_ENV_MYSQL_USER,
-    password: process.env.MYSQL_ENV_MYSQL_PASSWORD,
-    database: process.env.MYSQL_ENV_MYSQL_DATABASE,
-    multipleStatements: true,
-  },
-  mariadb: {
-    host: url.parse(process.env.MARIADB_PORT || '').hostname,
-    port: parseInt(url.parse(process.env.MARIADB_PORT || '').port, 10),
-    user: process.env.MARIADB_ENV_MARIADB_USER,
-    password: process.env.MARIADB_ENV_MARIADB_PASSWORD,
-    database: process.env.MARIADB_ENV_MARIADB_DATABASE,
-    multipleStatements: true,
-  },
-  sqlserver: {
-    host: process.env.SQLSERVER_ENV_SQLSERVER_HOST,
-    port: parseInt(process.env.SQLSERVER_ENV_SQLSERVER_PORT, 10),
-    user: process.env.SQLSERVER_ENV_SQLSERVER_USER,
-    password: process.env.SQLSERVER_ENV_SQLSERVER_PASSWORD,
-    database: process.env.SQLSERVER_ENV_SQLSERVER_DATABASE,
-  },
+const dbs = {
   sqlite: {
     database: path.join(__dirname, 'sqlite', 'sqlectron.db'),
   },
-  cassandra: {
-    host: url.parse(process.env.CASSANDRA_PORT || '').hostname,
-    port: 9042,
-    database: 'sqlectron',
-  },
 };
+
+if (process.env.POSTGRES_DSN) {
+  const postgres = new ConnectionString(process.env.POSTGRES_DSN);
+  dbs.postgresql = {
+    host: postgres.hosts[0].name,
+    port: postgres.hosts[0].port || 5432,
+    user: postgres.user || 'postgres',
+    password: postgres.password || '',
+    database: postgres.params.dbname || 'sqlectron',
+  };
+}
+
+if (process.env.MYSQL_DSN) {
+  const mysql = new ConnectionString(process.env.MYSQL_DSN);
+  dbs.mysql = {
+    host: mysql.hosts[0].name,
+    port: mysql.hosts[0].port || 3306,
+    user: mysql.user || 'root',
+    password: mysql.password || '',
+    database: mysql.params.dbname || 'sqlectron',
+  };
+}
+
+if (process.env.MARIADB_DSN) {
+  const mariadb = new ConnectionString(process.env.MARIADB_DSN);
+  dbs.mariadb = {
+    host: mariadb.hosts[0].name,
+    port: mariadb.hosts[0].port || 3306,
+    user: mariadb.user || 'root',
+    password: mariadb.password || '',
+    database: mariadb.params.dbname || 'sqlectron',
+  };
+}
+
+if (process.env.SQLSERVER_DSN) {
+  const sqlserver = new ConnectionString(process.env.SQLSERVER_DSN);
+  dbs.sqlserver = {
+    host: sqlserver.hosts[0].name,
+    port: sqlserver.hosts[0].port || 1433,
+    user: sqlserver.user || 'sa',
+    password: sqlserver.password || '',
+    database: sqlserver.params.dbname || 'sqlectron,',
+  };
+}
+
+if (process.env.CASSANDRA_DSN) {
+  const cassandra = new ConnectionString(process.env.CASSANDRA_DSN);
+  dbs.cassandra = {
+    host: cassandra.hosts[0].name,
+    port: cassandra.hosts[0].port || 9042,
+    database: cassandra.params ? cassandra.params.dbname || 'sqlectron' : 'sqlectron',
+  };
+}
+
+export default dbs;
