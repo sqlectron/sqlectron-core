e5f06a6d8d62f752e3d21f3ed8214a7ef19d4fff
Matthew Peveler <matt.peveler@gmail.com>
2020-08-06 12:39:53 -0400

    Simplify CI environment to match test config defaults (#105)
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- spec/databases/config.js
+++ spec/databases/config.js
@@ -7,75 +7,74 @@ const dbs = {
   },
 };
 
-if (process.env.POSTGRES_DSN) {
-  const postgres = new ConnectionString(process.env.POSTGRES_DSN, {
-    user: 'postgres',
-    password: '',
-    path: ['sqlectron'],
-  });
-  dbs.postgresql = {
-    host: postgres.hostname,
-    port: postgres.port || 5432,
-    user: postgres.user,
-    password: postgres.password,
-    database: postgres.path && postgres.path[0],
-  };
-}
+const postgres = new ConnectionString(process.env.POSTGRES_DSN || '', {
+  protocol: 'postgres',
+  user: 'postgres',
+  password: 'Password12!',
+  path: ['sqlectron'],
+  hosts: [{ name: 'localhost', port: 5432 }],
+});
+dbs.postgresql = {
+  host: postgres.hostname,
+  port: postgres.port || 5432,
+  user: postgres.user,
+  password: postgres.password,
+  database: postgres.path && postgres.path[0],
+};
 
-if (process.env.MYSQL_DSN) {
-  const mysql = new ConnectionString(process.env.MYSQL_DSN, {
-    user: 'root',
-    password: '',
-    path: ['sqlectron'],
-  });
-  dbs.mysql = {
-    host: mysql.hostname,
-    port: mysql.port || 3306,
-    user: mysql.user,
-    password: mysql.password,
-    database: mysql.path && mysql.path[0],
-  };
-}
+const mysql = new ConnectionString(process.env.MYSQL_DSN || '', {
+  protocol: 'mysql',
+  user: 'root',
+  password: 'Password12!',
+  path: ['sqlectron'],
+  hosts: [{ name: 'localhost', port: 3306 }],
+});
+dbs.mysql = {
+  host: mysql.hostname,
+  port: mysql.port || 3306,
+  user: mysql.user,
+  password: mysql.password,
+  database: mysql.path && mysql.path[0],
+};
 
-if (process.env.MARIADB_DSN) {
-  const mariadb = new ConnectionString(process.env.MARIADB_DSN, {
-    user: 'root',
-    password: '',
-    path: ['sqlectron'],
-  });
-  dbs.mariadb = {
-    host: mariadb.hostname,
-    port: mariadb.port || 3306,
-    user: mariadb.user,
-    password: mariadb.password,
-    database: mariadb.path && mariadb.path[0],
-  };
-}
+const mariadb = new ConnectionString(process.env.MARIADB_DSN || '', {
+  user: 'root',
+  password: 'Password12!',
+  path: ['sqlectron'],
+  hosts: [{ name: 'localhost', port: 3307 }],
+});
+dbs.mariadb = {
+  host: mariadb.hostname,
+  port: mariadb.port || 3307,
+  user: mariadb.user,
+  password: mariadb.password,
+  database: mariadb.path && mariadb.path[0],
+};
 
-if (process.env.SQLSERVER_DSN) {
-  const sqlserver = new ConnectionString(process.env.SQLSERVER_DSN, {
-    user: 'sa',
-    password: '',
-    path: ['sqlectron'],
-  });
-  dbs.sqlserver = {
-    host: sqlserver.hostname,
-    port: sqlserver.port || 1433,
-    user: sqlserver.user,
-    password: sqlserver.password,
-    database: sqlserver.path && sqlserver.path[0],
-  };
-}
+const sqlserver = new ConnectionString(process.env.SQLSERVER_DSN || '', {
+  protocol: 'mssql',
+  user: 'sa',
+  password: 'Password12!',
+  path: ['sqlectron'],
+  hosts: [{ name: 'localhost', port: 1433 }],
+});
+dbs.sqlserver = {
+  host: sqlserver.hostname,
+  port: sqlserver.port || 1433,
+  user: sqlserver.user,
+  password: sqlserver.password,
+  database: sqlserver.path && sqlserver.path[0],
+};
 
-if (process.env.CASSANDRA_DSN) {
-  const cassandra = new ConnectionString(process.env.CASSANDRA_DSN, {
-    path: ['sqlectron'],
-  });
-  dbs.cassandra = {
-    host: cassandra.hostname,
-    port: cassandra.port || 9042,
-    database: cassandra.path && cassandra.path[0],
-  };
-}
+const cassandra = new ConnectionString(process.env.CASSANDRA_DSN || '', {
+  protocol: 'cassandra',
+  path: ['sqlectron'],
+  hosts: [{ name: 'localhost', port: 9042 }],
+});
+dbs.cassandra = {
+  host: cassandra.hostname,
+  port: cassandra.port || 9042,
+  database: cassandra.path && cassandra.path[0],
+};
 
 export default dbs;
