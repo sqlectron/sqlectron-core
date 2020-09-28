6c3f9577676df7233a2dffedbf50237f6321fe86
Thomas Petersson <61778143+thomaspet@users.noreply.github.com>
2020-09-28 20:05:35 +0200

    remove duplicate database.connecting check (#115)

--- src/db/client.js
+++ src/db/client.js
@@ -48,10 +48,6 @@ export function createConnection(server, database) {
 async function connect(server, database) {
   /* eslint no-param-reassign: 0 */
   if (database.connecting) {
-    throw new Error('There is already a connection in progress for this server. Aborting this new request.');
-  }
-
-  if (database.connecting) {
     throw new Error('There is already a connection in progress for this database. Aborting this new request.');
   }
 
